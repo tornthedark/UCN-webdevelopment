09596588519c0fc30a07c0d57387bbe14f4c42ec
tornthedark <kimbach@hotmail.com>
2017-04-12 03:04:58 +0200

    Some changes to server.js

--- assignment1/server.js
+++ assignment1/server.js
@@ -36,8 +36,8 @@ app.get('/setup', function (req, res) {
 
     // create a sample user
     var nick = new User({
-        name: 'Kim Bach',
-        password: 'password',
+        name: 'test',
+        password: 'pass',
         admin: true
     });
 
@@ -69,14 +69,12 @@ apiRoutes.post('/authenticate', function (req, res) {
             if (user.password != req.body.password) {
                 res.json({ success: false, message: 'Authentication failed. Wrong password.' });
             } else {
-
                 // if user is found and password is right
                 // create a token
                 var token = jwt.sign(user, app.get('superSecret'), {
-                    expiresInMinutes: 1440 // expires in 24 hours
+                    expiresIn: 1440 // expires in 24 hours
                 });
-
-                // return the information including token as JSON
+                
                 res.json({
                     success: true,
                     message: 'Enjoy your token!',
@@ -88,10 +86,34 @@ apiRoutes.post('/authenticate', function (req, res) {
     });
 });
 
+// route middleware to verify a token
+apiRoutes.use(function (req, res, next) {
 
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
 
-// TODO: route middleware to verify a token
+    } else {
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
+});
 
 // route to show a random message (GET http://localhost:8080/api/)
 apiRoutes.get('/', function (req, res) {
